dece0debc1d69a1affd18d288cb99017ad6b6c02
Vidhi Mehta <42714739+VidhiJMehta@users.noreply.github.com>
2021-02-28 09:35:31 -0600

    Update getThemGreens.py

--- getThemGreens.py
+++ getThemGreens.py
@@ -2,7 +2,6 @@
 """
 Created on Sun Feb 28 03:39:31 2021
 
-@author: vjm180001
 """
 
 
@@ -10,21 +9,62 @@ from pathlib import Path
 import streamlit as st
 import numpy as np
 import pandas as pd
-
+from PIL import Image
+import base64
 
 
 df2 = pd.read_csv('./querytodo.csv', sep = ',')
-st.title('My first app')
-st.write(df2.head())
+del df2 ["freezeproof"]
 
 import zipfinder
+
+
+#st.sidebar.image("https://github.com/arunima1820/HackUTDVII/blob/main/getthemgreens.PNG")
+#st.sidebar.image("https://github.com/arunima1820/HackUTDVII/blob/main/get-them-greens/public/favicon.png")
+favi = Image.open("./getthemgreens.png")
+temoc = Image.open("./temochome.gif")
+st.image(favi)
+st.sidebar.image(temoc)
+
+
+my_table = st.empty()
+
 zip_input = st.text_input(" ")
 button_clicked = st.button("Enter ZIP code!")
 [city, zone] = zipfinder.search_zip(zip_input)
 st.write("Based on your Zip Code, we found that you live in " + city.title() + " and your USDA Plant Hardiness Zone is " + str(zone) + '.')
 
-#chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
-#st.line_chart(chart_data)
-option = st.selectbox('Which number do you like best?',df2['Name'])
+df3 = zipfinder.parseZone(str(zone))
+st.write("Here is a list of plants that you can grow in your zone!")
 
-'You selected: ', option
+st.sidebar.subheader("What skillset do you have with gardening?")
+skill = st.sidebar.radio(label="Select a skill level", options=["Beginner", "Advanced"], key=[0,1])
+df3 = zipfinder.parseSkillLevel(skill, df3)
+
+st.sidebar.subheader("Display only freeze-resistant plants?")
+freezeproof = 1 if st.sidebar.checkbox('Yes!') else 0
+df3 = zipfinder.parseFreezeLevel(freezeproof, df3)
+
+st.sidebar.subheader("Where will you be growing the plants?")
+locations = st.sidebar.multiselect(label="Select location", options=["Indoors", "Outdoors"])
+#locations
+df3 = zipfinder.parseIndoors(locations, df3)
+
+st.sidebar.subheader("How much space do you have?")
+space_select = st.sidebar.radio("Please specify", ('Not much', 'Decent', 'Tons'))
+df3 = zipfinder.parseSpace(space_select, df3)
+
+st.sidebar.subheader("Select the max number of months you are willing to wait until harvest")
+harvest_time = st.sidebar.select_slider(label="Slide to select",value=2, options=[1,2,3,4,5,6,7,8,9,10,11,12])
+df3 = zipfinder.parseHarvest(harvest_time, df3)
+
+submit_status = st.sidebar.button(label="Submit")
+if submit_status:
+    st.write(df3)
+opts = np.asarray(df3["Name"])
+user_selected_plants = st.multiselect(label="Select the plants you'd be interested in harvesting", options=opts)
+#user_selected_plants
+check_conflict_status = st.button(label="Check Conflicts")
+if check_conflict_status:
+    conflict_list = zipfinder.checkEnemies(df3)
+    conflict_list
